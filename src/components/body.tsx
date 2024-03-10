39123ea42ea6150335c774289364ac76cc77147e
George Raafat <georgexraafat@gmail.com>
2024-03-10 22:33:31 +0200

    add link to the logo

--- src/components/body.tsx
+++ src/components/body.tsx
@@ -2,6 +2,7 @@ import { signOut } from "@/auth";
 import { Button } from "@/components/ui/button";
 import { Caveat_Brush } from "next/font/google";
 import { Inter } from "next/font/google";
+import Link from "next/link";
 
 const caveat = Caveat_Brush({
   subsets: ["latin"],
@@ -20,11 +21,13 @@ export const Body = ({ children, showUser }: BodyProps) => {
   return (
     <body className={`${inter.className} flex flex-col min-h-screen`}>
       <header className="text-3xl font-bold bg-lime-50 space-x-4 flex justify-between align-baseline w-full p-5">
-        <h1
-          className={`bg-gradient-to-r from-blue-600 via-red-400 to-green-600 inline-block text-transparent bg-clip-text text-6xl m-3  ${caveat.className}`}
-        >
-          Doodles
-        </h1>
+        <Link href="/">
+          <h1
+            className={`bg-gradient-to-r from-blue-600 via-red-400 to-green-600 inline-block text-transparent bg-clip-text text-6xl m-3  ${caveat.className}`}
+          >
+            Doodles
+          </h1>
+        </Link>
         {showUser && (
           <div className="flex items-center gap-x-4">
             <form
