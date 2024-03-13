484acff929053f120b7a197c2d3e8b0267640516
George Raafat <georgexraafat@gmail.com>
2024-03-13 02:03:26 +0200

    add sign in with google

--- src/components/auth/social.tsx
+++ src/components/auth/social.tsx
@@ -3,15 +3,23 @@
 import { FcGoogle } from "react-icons/fc";
 import { FaFacebook } from "react-icons/fa";
 import { Button } from "../ui/button";
+import { DEFAULT_LOGIN_REDIRECT } from "@/routes";
+import { signIn } from "next-auth/react";
 
 export const Social = () => {
   return (
     <div className="flex items-center gap-x-2 w-full">
-      <Button size="lg" variant="outline" className="w-full" onClick={() => {}}>
+      <Button
+        size="lg"
+        variant="outline"
+        className="w-full"
+        onClick={() =>
+          signIn("google", {
+            callbackUrl: DEFAULT_LOGIN_REDIRECT,
+          })
+        }
+      >
         <FcGoogle className="h-5 w-5"></FcGoogle>
-      </Button>
-      <Button size="lg" variant="outline" className="w-full" onClick={() => {}}>
-        <FaFacebook className="h-5 w-5 text-[#1877F2]"></FaFacebook>
       </Button>
     </div>
   );
