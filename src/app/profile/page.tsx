31cf8f08fa53e64f2aba0d2421c1878aa5a37990
Mennatalla-Khougha <menmen.khougha@gmail.com>
2024-03-15 11:37:33 +0200

    The Upload page

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -9,35 +9,14 @@ import FilterComponent from "@/components/filters";
 export default function Profile() {
   return (
     <Body showUser={true}>
-      <div className="flex w-3/4 mx-auto space-x-8">
-        <div className="flex flex-col w-3/4 mx-auto space-x-4 space-y-4">
-          <div className="flex justify-center">
-            <FilterComponent />
-          </div>
-          <div
-            className="grid 
-              grid-cols-1 
-              md:grid-cols-2 
-              2xl:grid-cols-3
-              gap-4
-              p-4
-              bg-orange-200
-              relative
-              min-h-screen
-              "
-          >
-            <HomePageCards />
-            <HomePageCards />
-            <HomePageCards />
-            <HomePageCards />
-          </div>
-        </div>
+      <div className="flex mx-auto space-x-8 sm:w-3/4">
+        <div className="flex flex-col w-full mx-auto space-y-4">
         <div className="flex flex-col space-y-4 items-center">
           <h1 className="text-2xl font-semibold">Profile</h1>
           <img
             src="https://placehold.co/400x400"
             alt="Placeholder image"
-            className="rounded-full w-24 h-24"
+            className="rounded-full w-24 h-24 sm:w-36 sm:h-36"
           />
           <div className="flex justify-center">
             <CldUploadWidget
@@ -68,6 +47,26 @@ export default function Profile() {
             </CldUploadWidget>
           </div>
         </div>
+          <div className="flex justify-center">
+            <FilterComponent />
+          </div>
+          <div
+            className="grid 
+              grid-cols-1 
+              lg:grid-cols-2 
+              gap-4
+              p-4
+              relative
+              min-h-screen
+              "
+          >
+            <HomePageCards />
+            <HomePageCards />
+            <HomePageCards />
+            <HomePageCards />
+          </div>
+        </div>
+
       </div>
     </Body>
   );
