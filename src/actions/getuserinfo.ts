aaa5f5bd36925e37194253823702474fba4fb708
George Raafat <georgexraafat@gmail.com>
2024-03-15 10:55:05 +0200

    auto update profile pic

--- src/actions/getuserinfo.ts
+++ src/actions/getuserinfo.ts
@@ -1,10 +1,19 @@
 "use server";
 import { auth } from "@/auth";
+import { db } from "@/lib/db";
 
 export const getUserImage = async () => {
   const session = await auth();
-  if (!session?.user?.image) {
+  const user = await db.user.findUnique({
+    where: {
+      id: session?.user?.id,
+    },
+    select: {
+      image: true,
+    },
+  });
+  if (!user?.image) {
     return "profilepic";
   }
-  return session.user.image;
+  return user.image;
 };
