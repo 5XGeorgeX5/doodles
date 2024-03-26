2d073d8640603a353d9d75b8c71bb04ee571e4ac
Mennatalla-Khougha <menmen.khougha@gmail.com>
2024-03-26 11:18:50 +0200

    cleaning the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,4 @@
-import { HomePageCards } from "@/components/homePageCards";
 import { Body } from "@/components/body";
-import { FilterComponent } from "@/components/filters";
 import { getAllDrawings } from "@/data/drawing";
 import { getUserInfo } from "@/actions/getuserinfo";
 import { getUserRatings } from "@/data/rating";
